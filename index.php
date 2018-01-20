b6ed7513f9866b23de560ab12bbead02c01f8ee9
codeandcreate <info@codeandcreate.de>
2018-01-20 18:25:09 +0100

    dynamic site configuration added

--- index.php
+++ index.php
@@ -1,16 +1,50 @@
 <?
 	$requestPices = explode("/", substr($_SERVER['REQUEST_URI'], 1));
-	
+
+	if (file_exists(dirname(__FILE__)."/site/dynamic/ccCMS_dynamic.conf.php")) {
+		include dirname(__FILE__)."/site/dynamic/ccCMS_dynamic.conf.php";
+		if (isset($ccCMS_dynamicConf)) {
+			if (empty($requestPices[0]) && isset($ccCMS_dynamicConf['home'])) {
+				header("LOCATION: /" . $ccCMS_dynamicConf['home']);
+				die();
+			} else if (!empty($requestPices[0])) {
+				if (isset($ccCMS_dynamicConf['pages'][$requestPices[0]])) {
+					if (file_exists(dirname(__FILE__)."/site/dynamic/" . $ccCMS_dynamicConf['pages'][$requestPices[0]])) {
+						include dirname(__FILE__)."/site/dynamic/" . $ccCMS_dynamicConf['pages'][$requestPices[0]];
+						die();
+					}
+				} else if (in_array($requestPices[0], $ccCMS_dynamicConf['directFiles'])) {
+					if (file_exists(dirname(__FILE__)."/site/" . $requestPices[0])) {
+						readfile (dirname(__FILE__)."/site/" . $requestPices[0]);
+						die();
+					}		
+				}
+			}
+		}
+	}
+
 	switch($requestPices[0]) {
 		case 'css':
 		case 'js':
 		case 'img':
+		case 'font':
 			$fileToServe 	= dirname(__FILE__) . "/site" . $_SERVER['REQUEST_URI'];
 			if (is_file($fileToServe)) {
-				$mimetype = mime_content_type($fileToServe);
+				if ($requestPices[0] === "css") {
+					header('Content-type: text/css');
+				} else {
+					$mimetype = mime_content_type($fileToServe);
+					header('Content-type: ' . $mimetype);
+				}
 				$filesize = filesize($fileToServe);
-				header('Content-type: ' . $mimetype);
 				header('Content-Length: ' . $filesize);
+				
+				$seconds_to_cache = 3600;
+				$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+				header("Expires: $ts");
+				header("Pragma: cache");
+				header("Cache-Control: max-age=$seconds_to_cache");
+				
 				readfile($fileToServe);
 				die();
 			}
@@ -33,4 +67,3 @@
 			}
 			die();
 	}
-	print_r($requestPices);
---
+++ site/dynamic/ccCMS_dynamic.conf.php
@@ -0,0 +1,14 @@
+<?
+	$ccCMS_dynamicConf = [
+		/* dynamic pages based on path */
+		'pages' => [
+			//'blog' => 'blog.php',
+			//'impress' => 'impress.php'
+		],
+		/* files that should be served directly form /site */
+		'directFiles' => [
+			//'sitemap.xml'	
+		],
+		/* if set, this is the default url of the site */
+		//'home' => 'blog'
+	];
